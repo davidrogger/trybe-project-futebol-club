2d7bdf7cee562083a5edad17b9c57391550eea3b
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-22 16:44:07 -0300

    feat: remote test

--- app/backend/src/interfaces/Match.interface.ts
+++ app/backend/src/interfaces/Match.interface.ts
@@ -2,7 +2,7 @@ export interface ITeamGoals {
   homeTeamGoals: number;
   awayTeamGoals: number;
 }
-
+// remote unknown fail
 export interface InewMatch extends ITeamGoals {
   homeTeam: number;
   awayTeam: number;
