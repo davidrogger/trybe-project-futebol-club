415578587c36f5496ad5c0e68e6a6eacfca5912a
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-23 16:05:59 -0300

    test: add 2 more tests
    test with a mocked data testing key fields from the response

--- app/backend/src/tests/getLeaderboard.test.ts
+++ app/backend/src/tests/getLeaderboard.test.ts
@@ -7,6 +7,8 @@ import { Response } from 'superagent';
 import { app } from '../app';
 import MatchModel from '../database/models/MatchModel';
 import { matchModelMock } from './mockedData/sequelizeMock';
+import { ITeamBoard } from '../interfaces/Leaderboard.interface';
+import { leaderBoardMock } from './mockedData/leaderBoardMock';
 
 chai.use(chaiHttp);
 
@@ -25,6 +27,32 @@ describe('Route "/leaderboard"', () => {
     
     it('Should return status 200', () => {
       expect(response).to.have.status(200);
+    });
+
+    it('Should return a list with a pattern display fields', () => {
+      const allTeamsBoard = response.body;
+      expect(allTeamsBoard[0]).to.includes.keys(
+        'name', 'totalPoints', 'totalGames', 'totalVictories', 'totalDraws',
+        'totalLosses', 'goalsFavor', 'goalsOwn', 'goalsBalance', 'efficiency',
+        );
+    })
+
+    it('Should order the list by its "totalPoints" follow by "totalVictories", \
+    "goalsBalance", "goalsFavor", "goalsOwn"', () => {
+      const allTeamsBoard: ITeamBoard[] = response.body;
+
+      allTeamsBoard.forEach((team, index) => {
+        expect(team.name).to.be.equal(leaderBoardMock[index].name);
+        expect(team.totalPoints).to.be.equal(leaderBoardMock[index].totalPoints);
+        expect(team.totalGames).to.be.equal(leaderBoardMock[index].totalGames);
+        expect(team.totalVictories).to.be.equal(leaderBoardMock[index].totalVictories);
+        expect(team.totalDraws).to.be.equal(leaderBoardMock[index].totalDraws);
+        expect(team.totalLosses).to.be.equal(leaderBoardMock[index].totalLosses);
+        expect(team.goalsFavor).to.be.equal(leaderBoardMock[index].goalsFavor);
+        expect(team.goalsOwn).to.be.equal(leaderBoardMock[index].goalsOwn);
+        expect(team.goalsBalance).to.be.equal(leaderBoardMock[index].goalsBalance);
+        expect(team.efficiency).to.be.equal(leaderBoardMock[index].efficiency);
+      });
     });
   });
   
---
+++ app/backend/src/tests/mockedData/leaderBoardMock.ts
@@ -0,0 +1,38 @@
+export const leaderBoardMock = [
+  {
+    totalPoints: 4,
+    totalGames: 3,
+    name: 'São Paulo',
+    totalVictories: 1,
+    totalDraws: 1,
+    totalLosses: 1,
+    goalsFavor: 4,
+    goalsOwn: 4,
+    goalsBalance: 0,
+    efficiency: '44.44'
+  },
+  {
+    totalPoints: 3,
+    totalGames: 1,
+    name: 'Ferroviária',
+    totalVictories: 1,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 2,
+    goalsOwn: 1,
+    goalsBalance: 1,
+    efficiency: '100.00'
+  },
+  {
+    totalPoints: 1,
+    totalGames: 1,
+    name: 'Internacional',
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  }
+]
