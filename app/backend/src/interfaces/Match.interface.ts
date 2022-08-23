1516ef92713e5ff67bd58d53070e4771deaebde4
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-23 16:15:43 -0300

    chore: remove commnet

--- app/backend/src/interfaces/Match.interface.ts
+++ app/backend/src/interfaces/Match.interface.ts
@@ -4,7 +4,7 @@ export interface ITeamGoals {
   homeTeamGoals: number;
   awayTeamGoals: number;
 }
-// remote unknown fail
+
 export interface InewMatch extends ITeamGoals {
   homeTeam: number;
   awayTeam: number;
