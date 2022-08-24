312005818dcf19fc33db69927edacd0103d3c2d9
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-24 14:44:56 -0300

    chore: :fire: remove method
    remove unnecessary method that create a hashpassword

--- app/backend/src/services/PasswordHash.service.ts
+++ app/backend/src/services/PasswordHash.service.ts
@@ -1,10 +1,6 @@
 import * as bcrypt from 'bcryptjs';
 
 class PasswordHash {
-  static encrypt(password: string): string {
-    return bcrypt.hashSync(password);
-  }
-
   static verify(password: string, passwordHash: string): boolean {
     return bcrypt.compareSync(password, passwordHash);
   }
