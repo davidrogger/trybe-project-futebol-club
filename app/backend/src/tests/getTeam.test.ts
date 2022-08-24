e64e3be6f19228cf2b06bc60cdd5cdd46490aef5
Davíd Roggér <davidrogger.a@gmail.com>
2022-08-24 15:19:50 -0300

    test: :sparkles: route get Team
    add test when id is pass wrongly and when the id dont exists in the database add calls fakes to the tests

--- app/backend/src/tests/getTeam.test.ts
+++ app/backend/src/tests/getTeam.test.ts
@@ -9,6 +9,7 @@ chai.use(chaiHttp);
 
 import TeamModel from '../database/models/TeamModel';
 import { mockedTeam, mockedTeamList } from './mockedData/TeamMock';
+import { teamModelMock } from './mockedData/sequelizeMock';
 
 const { stub } = sinon;
 const { expect } = chai;
@@ -27,11 +28,49 @@ describe('route /teams', () => {
   });
 
   describe('When use route "/teams/id"', () => {
-    it('Should get a team by its "id"', async () => {
-      stub(TeamModel, 'findByPk').resolves(mockedTeam as TeamModel);
-      response = await chai.request(app).get('/teams/1');
-      expect(response).to.have.status(200);
-      expect(response.body).to.be.deep.equal(mockedTeam);
+
+    describe('When the id is fullfilled correcly' , () => {
+      before(async () => {
+        stub(TeamModel, 'findByPk').callsFake(teamModelMock.findByPk);
+        response = await chai.request(app).get('/teams/1');
+      });
+
+      it('Should response 200', () => {
+        expect(response).to.have.status(200);
+      });
+      it('Should show the team related to that id', async () => {        
+        expect(response.body).to.be.deep.equal(mockedTeam);
+      });
+    });
+
+    describe('When the id is fullfilled wrongly', () => {
+      before(async () => {
+        response = await chai.request(app).get('/teams/a');
+      });
+
+      it('Should response status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('Should response "Id must be a number"', () => {
+        expect(response.body.message).to.be.equal('Id must be a number');
+      });
+
+    });
+
+    describe('When the id dont exists in the database', () => {
+      before(async () => {
+        stub(TeamModel, 'findByPk').callsFake(teamModelMock.findByPk);
+        response = await chai.request(app).get('/teams/100');
+      });
+
+      it('Should return status 404', () => {
+        expect(response).to.have.status(404);
+      });
+
+      it('Should return message "There is no team with such id!"', () => {
+        expect(response.body.message).to.be.equal('There is no team with such id!');
+      });
     });
   });
 })
