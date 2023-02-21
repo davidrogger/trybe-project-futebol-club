dbf560f9d2804d40cb5a1fb616cc82279c11c94a
davidrogger <davidrogger.a@gmail.com>
2023-02-21 14:00:15 -0300

    FEAT: throw error when is missing a newMatchBody field

--- app/backend/src/services/InputValidator.service.ts
+++ app/backend/src/services/InputValidator.service.ts
@@ -28,6 +28,9 @@ class InputValidator {
 
   static newMatch(match: InewMatch): InewMatch {
     const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = match;
+    if ([homeTeam, awayTeam, homeTeamGoals, awayTeamGoals].some((field) => field === undefined)) {
+      throw new InvalidInputError('All fields must be filled');
+    }
     if (homeTeam === awayTeam) {
       throw new UnauthorizedError('It is not possible to create a match with two equal teams');
     }
